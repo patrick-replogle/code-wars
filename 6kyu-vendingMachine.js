3c18d45b6e4bb47de9e87b7ad6ed2e44d0abc6dc
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-08-12 12:45:43 -0700

    Update 6kyu-vendingMachine.js

--- 6kyu-vendingMachine.js
+++ 6kyu-vendingMachine.js
@@ -2,32 +2,37 @@ class VendingMachine {
   constructor(items, money) {
     this.items = items
     this.money = money 
-  }
+  } 
   
   vend(selection, itemMoney) {
+    // first search selection this.items
     let found = this.items.filter((val, index) => selection === val.code)
     
-    // check if item is out of stock
-    if(found.length > 0 && found[0]['quantity'] === 0) return `${found[0]['name']}: Out of stock!`
+    // if not found return invalid selection
+    if(found.length === 0) {
+      return `Invalid selection! : Money in vending machine = ${(this.money).toFixed(2)}`
+      
+    // else time to check for other validations
+    } else {
+      // check if customer didn't insert enough money
+      if(itemMoney < found[0]['price']) return 'Not enough money!'
+      
+      // check if item is out of stock
+      if(found[0]['quantity'] === 0) return `${found[0]['name']}: Out of stock!`
     
-    // check if vending machine doesn't have enough money to give change
-    if(found.length === 0) return `Invalid selection! : Money in vending machine = ${(this.money).toFixed(2)}`
-    
-    // check if customer didn't insert enough money
-    if(found.length > 0 && itemMoney < found[0]['price']) return 'Not enough money!'
-    
-    // vend item if customer gave exact change
-    if(found.length > 0 && itemMoney === found[0]['price']) {
-      this.money += itemMoney
-      found[0]['quantity'] -= 1
-      return `Vending ${found[0]['name']}`
-    }
-    
-    // vend item and give change is customer overpaid
-    if(found.length > 0 && itemMoney > found[0]['price']) {
-      this.money += found[0]['price']
-      found[0]['quantity'] -= 1
-      return `Vending ${found[0]['name']} with ${(itemMoney - found[0]['price']).toFixed(2)} change.`
-    }  
+      // vend item if customer gave exact change
+      if(itemMoney === found[0]['price']) {
+        this.money += itemMoney
+        found[0]['quantity'] -= 1
+        return `Vending ${found[0]['name']}`
+      }
+      
+      // vend item and give change if customer overpaid
+      if(itemMoney > found[0]['price']) {
+        this.money += found[0]['price']
+        found[0]['quantity'] -= 1
+        return `Vending ${found[0]['name']} with ${(itemMoney - found[0]['price']).toFixed(2)} change.`
+      }    
+    } 
   }
 }
